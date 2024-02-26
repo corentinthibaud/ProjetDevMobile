09cc1b5556e44f9fdd063a8472c698f4afdef4e3
Remi-Vassoille <78744690+Remi-Vassoille@users.noreply.github.com>
2024-02-26 14:42:10 +0100

    Added composable for Sign In and Sign Up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.2"
+        kotlinCompilerExtensionVersion = "1.5.1"
 
     }
     kotlinOptions {
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.activity:activity-compose:1.3.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("com.google.android.material:material:1.11.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
--- app/src/main/java/fr/isen/m1/devlogiciel/projetdevmobile/activity/AuthenticationActivity.kt
+++ app/src/main/java/fr/isen/m1/devlogiciel/projetdevmobile/activity/AuthenticationActivity.kt
@@ -2,12 +2,31 @@ package fr.isen.m1.devlogiciel.projetdevmobile.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.activity.compose.setContent
+import androidx.compose.ui.tooling.preview.Preview
 import fr.isen.m1.devlogiciel.projetdevmobile.R
 
 class AuthenticationActivity : AppCompatActivity() {
     private lateinit var authFirebase : FirebaseAuth
+    private var isSignedIn = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         authFirebase = Firebase.auth
@@ -16,12 +35,14 @@ class AuthenticationActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val currentUser = authFirebase.currentUser
-        if (currentUser != null) {
-            // User is signed in
-        } else {
-            // No user is signed in
+        isSignedIn = currentUser != null
+        if(!isSignedIn){
+            setContent{
+                SignInUp("signIn")
+            }
         }
     }
+
 
     private fun signUp(email: String, password: String) {
         authFirebase.createUserWithEmailAndPassword(email, password)
@@ -36,6 +57,7 @@ class AuthenticationActivity : AppCompatActivity() {
             }
     }
 
+
     private fun signIn(email: String, password: String) {
         // [START sign_in_with_email]
         authFirebase.signInWithEmailAndPassword(email, password)
@@ -49,5 +71,50 @@ class AuthenticationActivity : AppCompatActivity() {
                 }
             }
         // [END sign_in_with_email]
+    }
+
+    @Composable
+    fun SignInUp(typeOfConnexion: String) {
+        var email by remember { mutableStateOf(TextFieldValue()) }
+        var password by remember { mutableStateOf(TextFieldValue()) }
+
+        Column {
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+
+                keyboardActions = KeyboardActions(onDone = {
+                    if(typeOfConnexion == "signIn"){
+                        signIn(email.text, password.text)
+                    }else{
+                        signUp(email.text, password.text)
+                    }
+                })
+            )
+            Button(
+                onClick = {
+                    if(typeOfConnexion == "signIn"){
+                        signIn(email.text, password.text)
+                    }else{
+                        signUp(email.text, password.text)
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Sign In")
+            }
+        }
+
+
     }
 }
